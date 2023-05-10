1c25b126f51270357fed4e580a18fe123da020da
Paul Preda <ppd1512@gmail.com>
2023-05-10 11:17:03 +0100

    wrote the code for Express Restaurants Part 2

--- src/app.js
+++ src/app.js
@@ -5,10 +5,20 @@ const db = require("../db/connection");
 
 //TODO: Create your GET Request Route Below: 
 
+// part 1
 app.get("/restaurants", async (req, res) =>{
     let restaurants = await Restaurant.findAll()
     res.json(restaurants)
 })
+
+// part 2
+app.get("/restaurants/:id", async (req, res) => {
+    let id = req.params.id
+    let found = await Restaurant.findByPk(id)
+    res.json(found)
+})
+
+
 
 
 module.exports = app;
